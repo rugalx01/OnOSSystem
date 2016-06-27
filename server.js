a0975526010dc2ffe2d7cfcfba2cee52efc40934
OpenShift System User <576903377628e1b237000073@ex-std-node510.prod.rhcloud.com>
2016-06-27 06:12:45 -0400

    Ready to Start Server.js

--- server.js
+++ server.js
@@ -34,10 +34,17 @@ var FirstTestAPI = require('./Core/Api/FirstTestAPI');
 app.use('/api/v1', FirstTestAPI );
 
 // default is 127.0.0.1
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+//var server = app.listen(3000, function () {
+//  var host = server.address().address;
+//  var port = server.address().port;
+//  console.log('listening on %s:%s', host, port);
+//});
 
-  console.log('listening on %s:%s', host, port);
+//default on openshift
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 
